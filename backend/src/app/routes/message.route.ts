f55e495e58dd7d6d8f4e000436f3476ae8eb6858
Slv Sweqq <slv.sweqq@proton.me>
2025-11-18 18:36:48 +0600

    feat: implement sendMessage (route, controller)

--- backend/src/app/routes/message.route.ts
+++ backend/src/app/routes/message.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import type { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { getMessages } from "../controllers/message.controller";
+import { getMessages, sendMessage } from "../controllers/message.controller";
 
 const router: Router = express.Router();
 
 router.use(protectRoute);
 
 router.get("/:id", getMessages);
+
+router.post("/send/:id", sendMessage);
 
 export default router;
