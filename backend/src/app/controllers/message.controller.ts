b4834acbe18a77a2c7c00cc94716d7ede65e0967
Slv Sweqq <slv.sweqq@proton.me>
2025-11-18 14:58:34 +0600

    feat: implement GET users (route, controller)

---
+++ backend/src/app/controllers/message.controller.ts
@@ -0,0 +1,17 @@
+import type { Request, Response } from "express";
+import User from "../models/user.model";
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    // Get all users except yourself
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsers controller: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
