f5aa4c08934f75c1e7e593d8c49f316a0bb7a5d4
Slv Sweqq <slv.sweqq@proton.me>
2025-11-18 14:45:46 +0600

    feat: add message model and message router

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -3,6 +3,7 @@ import type { Express } from "express";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route";
+import messageRoutes from "./routes/message.route";
 
 const app: Express = express();
 
@@ -12,5 +13,6 @@ app.use(cookieParser());
 
 // ROUTES
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 export default app;
---
+++ backend/src/app/routes/message.route.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import type { Router } from "express";
+
+const router: Router = express.Router();
+
+export default router;
