25fe16be9c26ce3d5dd0bc8b818907bb050f0ebc
Slv Sweqq <slv.sweqq@proton.me>
2025-11-18 14:36:44 +0600

    fix: hashing password pre-hook in user model

--- backend/src/app/models/user.model.ts
+++ backend/src/app/models/user.model.ts
@@ -26,10 +26,16 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
+  if (!this.isModified("password")) return next();
+
+  try {
     // Hash password
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error: any) {
+    next(error);
   }
 });
 
