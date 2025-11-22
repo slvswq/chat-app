8e0e3386eb3ea75f4d262827fd21334c3696772f
Slv Sweqq <slv.sweqq@proton.me>
2025-11-22 22:45:56 +0600

    feat: set chat tab to channels on /create-channel page load

--- frontend/src/pages/CreateChannelPage.tsx
+++ frontend/src/pages/CreateChannelPage.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { CreateChannelForm } from "@/components/CreateChannelForm";
+import { useChatStore } from "@/store/useChatStore";
 
 const CreateChannelPage: React.FC = () => {
+  const { setCurrentTab } = useChatStore();
+
+  useEffect(() => {
+    setCurrentTab("channels");
+  }, [setCurrentTab]);
+
   return (
     <div className="w-full flex-1 bg-muted flex flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-lg flex-col gap-6">
